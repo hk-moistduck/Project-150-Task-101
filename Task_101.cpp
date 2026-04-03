46eb99d56168ed32918ca13cbf98eb0b5771f78e
Hasan Kabir <hasankabir125@gmail.com>
2026-04-04 02:49:04 +0600

    added comments to task_101

--- Task_101.cpp
+++ Task_101.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
-#include "./constants.h" //for defining all constants in a single place
+#include "./constants.h" // define all constants in a single place
 #include <math.h>
 
 
-//global variables
+// global variables
 int game_is_running = FALSE;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
@@ -15,10 +15,12 @@ struct circle{
 } circle;
 
 int initialize_window(void){
+    // initialize everything including video support, events etc
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0){
         fprintf(stderr,"Error initializing SDL\n");
         return FALSE;
     }
+    // create a SDL window
     window = SDL_CreateWindow(
         "task_101",
         SDL_WINDOWPOS_CENTERED,
@@ -31,7 +33,7 @@ int initialize_window(void){
         fprintf(stderr, "Error creating SDL Window\n");
         return FALSE;
     }
-
+    // createe a SDL renderer for rendering graphics in the window
     renderer = SDL_CreateRenderer(window, -1, 0);
     if(!renderer){
         fprintf(stderr, "Error creating SDL Renderer\n");
@@ -42,6 +44,7 @@ int initialize_window(void){
 }
 
 void setup(){
+    // define circle with center as the center of the window
     circle.x = WINDOW_WIDTH / 2;
     circle.y = WINDOW_HEIGHT / 2;
     circle.radius = 15;
@@ -49,7 +52,7 @@ void setup(){
 
 void process_input(){
     SDL_Event event;
-
+    // poll SDL events (e.g. closing by pressing escape key)
     while(SDL_PollEvent(&event)){
         switch(event.type){
             case SDL_QUIT:
@@ -57,6 +60,7 @@ void process_input(){
                 break;
             case SDL_KEYDOWN:
                 if(event.key.keysym.sym == SDLK_ESCAPE){
+                    // exist game loop by pressing escape key
                     game_is_running = FALSE;
                     break;
                 }
@@ -65,44 +69,49 @@ void process_input(){
 }
 
 void update(){
-    //to-do
+    // no update for this task
 }
-
+// draw circumference of the circle
 void draw_circle(){
+    // go around 360 degrees
     for(int degree = 0; degree <= 360; degree++){
+        // convert angles to radians
         float radian = degree * (M_PI / 180.0);
-
+        // coordinates of the point on the circle at each individual angles
         float x = circle.x + circle.radius * cos(radian);
         float y = circle.y + circle.radius * sin(radian);
-
+        // draw each point
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderDrawPoint(renderer, (int)round(x), (int)round(y));
     }
 }
-
+// draw inside circle
 void fill_circle(){
+    // go from bottom to top
     for(int y = -circle.radius; y <= circle.radius; y++){
+        // using pythagorean theorem to find x for each y
         int x = sqrt(circle.radius * circle.radius - y * y);
-
+        // draw each line
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderDrawLine(
             renderer,
-            circle.x - x, circle.y + y,
-            circle.x + x, circle.y + y
+            circle.x - x, circle.y + y, // left point
+            circle.x + x, circle.y + y // right point
         );
     }
 }
 
 void render(){
+    // set color and clear the screen with that color
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     draw_circle();
     fill_circle();
-    
+    // present the back buffer to the screen
     SDL_RenderPresent(renderer);
 }
-
+// destroy and clean up initialized systems after exiting game
 void destroy_window(){
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
